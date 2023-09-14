81381a28d93f25e832395bbdf8c773259a280c46
Niroyan Kugaruban <48611787+yanniro2@users.noreply.github.com>
2023-09-14 10:18:30 +0530

    readme.md file change

--- next.config.js
+++ next.config.js
@@ -1,27 +1,4 @@
 /** @type {import('next').NextConfig} */
-<<<<<<< HEAD
-const nextConfig = {}
-=======
-const nextConfig = {
-  // Enable React Strict Mode for development
-  reactStrictMode: true,
+const nextConfig = {};
 
-  // Enable SWC minification for production builds
-  swcMinify: true,
-
-  // Change the output directory for export
-  output: "my-exported-files",
-
-  // Add custom configuration options
-  customOption: "custom-value",
-
-  images: {
-    loader: "custom",
-    loaderFile: "./src/my-loader.ts", // Updated path to my-loader.ts
-  },
-};
->>>>>>> 96f3826 (first commit)
-
-module.exports = {
-  distDir: "out",
-};
+module.exports = nextConfig;
